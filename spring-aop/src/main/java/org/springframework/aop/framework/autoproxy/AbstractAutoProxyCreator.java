8a21e84630582ee5b0264d2d12b5c1afb3aa17fe
lichangbao <lichangbao@moqipobing.com>
2023-10-20 16:51:22 +0800

    2023年10月20日16:51:21

--- spring-aop/src/main/java/org/springframework/aop/framework/autoproxy/AbstractAutoProxyCreator.java
+++ spring-aop/src/main/java/org/springframework/aop/framework/autoproxy/AbstractAutoProxyCreator.java
@@ -316,6 +316,7 @@ public abstract class AbstractAutoProxyCreator extends ProxyProcessorSupport
 	public Object postProcessAfterInitialization(@Nullable Object bean, String beanName) {
 		if (bean != null) {
 			Object cacheKey = getCacheKey(bean.getClass(), beanName);
+			// 如果 beanName 对应 bean 存在循环依赖，依赖的一方在实例化时，已经把beanName对应的半成品（尚未初始化）的对象扔到this.earlyProxyReferences集合中了
 			if (this.earlyProxyReferences.remove(cacheKey) != bean) {
 				return wrapIfNecessary(bean, beanName, cacheKey);
 			}
