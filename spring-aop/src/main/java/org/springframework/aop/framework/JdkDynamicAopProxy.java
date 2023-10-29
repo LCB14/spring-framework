966e653b100d614094a9f0d84842937e13caea45
changbaol <1642272412@qq.com>
2023-10-29 18:20:33 +0800

    2023年10月29日18:20:32

--- spring-aop/src/main/java/org/springframework/aop/framework/JdkDynamicAopProxy.java
+++ spring-aop/src/main/java/org/springframework/aop/framework/JdkDynamicAopProxy.java
@@ -204,6 +204,7 @@ final class JdkDynamicAopProxy implements AopProxy, InvocationHandler, Serializa
 			Class<?> targetClass = (target != null ? target.getClass() : null);
 
 			// Get the interception chain for this method.
+			// 获取当前和指定参数匹配的拦截器（通知（advice））
 			List<Object> chain = this.advised.getInterceptorsAndDynamicInterceptionAdvice(method, targetClass);
 
 			// Check whether we have any advice. If we don't, we can fallback on direct
