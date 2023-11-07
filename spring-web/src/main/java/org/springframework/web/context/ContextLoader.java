23c8cef407447d5975d855b06235b57094abdc0f
lichangbao <lichangbao@moqipobing.com>
2023-11-07 15:30:36 +0800

    2023年11月07日15:30:35

--- spring-web/src/main/java/org/springframework/web/context/ContextLoader.java
+++ spring-web/src/main/java/org/springframework/web/context/ContextLoader.java
@@ -391,6 +391,26 @@ public class ContextLoader {
 		}
 
 		wac.setServletContext(sc);
+
+		/**
+		 * 	 CONFIG_LOCATION_PARAM 对应的属性值 /WEB-INF/Spring-mvc.xml
+		 *
+		 *   <context-param>
+		 *         <param-name>contextConfigLocation</param-name>
+		 *         <param-value>/WEB-INF/Spring-mvc.xml</param-value>
+		 *     </context-param>
+		 *
+		 *     <servlet>
+		 *         <servlet-name>app</servlet-name>
+		 *         <servlet-class>org.springframework.web.servlet.DispatcherServlet</servlet-class>
+		 *
+		 *         <init-param>
+		 *             <param-name>contextConfigLocation</param-name>
+		 *             <param-value></param-value>
+		 *         </init-param>
+		 *         <load-on-startup>1</load-on-startup>
+		 *     </servlet>
+		 */
 		String configLocationParam = sc.getInitParameter(CONFIG_LOCATION_PARAM);
 		if (configLocationParam != null) {
 			wac.setConfigLocation(configLocationParam);
